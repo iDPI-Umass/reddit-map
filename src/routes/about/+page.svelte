d6abd0a0d3ea40a6186197f045fb4e50fc808de0
David Harper <real.david.harper@gmail.com>
2023-05-09 17:19:49 -0700

    corrected background styling of about copy page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,7 @@
 
 <MainHeader></MainHeader>
 
-<Center>
+<main>
   <section class="gobo-copy">
     <h1>About RedditMap</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -16,4 +16,20 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
     
   </section>
-</Center>
+</main>
+
+<style>
+  main {
+    flex: 1 1 0;
+    min-height: 0;
+    max-width: 100vw;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    overflow-y: scroll;
+    background-color: var(--gobo-color-null);
+  }
+</style>
