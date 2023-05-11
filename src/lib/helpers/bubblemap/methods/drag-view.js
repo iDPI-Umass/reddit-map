24a60d53df158da3576644f419701b2e49448d43
David Harper <real.david.harper@gmail.com>
2023-05-10 18:39:34 -0700

    bubblemap drag now works on touch interfaces.

--- src/lib/helpers/bubblemap/methods/drag-view.js
+++ src/lib/helpers/bubblemap/methods/drag-view.js
@@ -1,9 +1,13 @@
-import { element } from "svelte/internal";
+let touchX = 0;
+let touchY = 0;
+
 
 const handlePointerDown = function ( event ) {
   event.preventDefault();
 
   this.element.onpointermove = this.dragView.bind( this );
+  this.element.ontouchstart = setTouch;
+  this.element.ontouchmove = this.dragView.bind( this );
   this.element.setPointerCapture( event.pointerId );
 };
 
@@ -11,14 +15,32 @@ const handlePointerUp = function ( event ) {
   event.preventDefault();
 
   this.element.onpointermove = null;
+  this.element.ontouchmove = null;
   this.element.releasePointerCapture( event.pointerId )
 };
 
-const dragView = function ( event ) {
-  event.preventDefault();
+const setTouch = function ( event ) {
+  const touch = event.touches[0];
+  touchX = touch.clientX;
+  touchY = touch.clientY;
+};
 
-  this.dragCenterX = this.dragCenterX - ( event.movementX * this.resolutionScale );
-  this.dragCenterY = this.dragCenterY - ( event.movementY * this.resolutionScale );
+const dragView = function ( event ) {
+  if ( event.type === "pointermove" ) {
+    event.preventDefault();
+    this.dragCenterX = this.dragCenterX - ( event.movementX * this.resolutionScale );
+    this.dragCenterY = this.dragCenterY - ( event.movementY * this.resolutionScale );
+
+  } else if ( event.type === "touchmove" ) {
+    const touch = event.touches[0];
+    const dx = touch.clientX - touchX;
+    const dy = touch.clientY - touchY;
+    touchX = touch.clientX;
+    touchY = touch.clientY;
+
+    this.dragCenterX = this.dragCenterX - ( dx * this.resolutionScale );
+    this.dragCenterY = this.dragCenterY - ( dy * this.resolutionScale );
+  }
 };
 
 
